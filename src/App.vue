d70e602690bd89d6ff1f12dc46d807114154ccdf
zch1999 <1901394767@qq.com>
2020-02-24 17:40:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container"></div>
+    <search />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Search from  './components/Search'  
+export default {
+  components:{
+    Search
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  padding: 0 15px;
+  text-align: center;
+}
+</style>
---
+++ src/views/Search.vue
@@ -0,0 +1,25 @@
+<!--  -->
+<template>
+  <div class="search">
+    搜索结果
+    <hr/>
+    {{this.$route.query.searchData}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+    }
+  },
+  mounted(){
+      console.log(this.$route.query.searchData)
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
